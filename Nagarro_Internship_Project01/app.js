9f8b3569c97f692bf335079b4baaac0f0117789e
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-08-11 07:42:08 +0530

    Initial Modifications in Twitter Clone

--- Nagarro_Internship_Project01/app.js
+++ Nagarro_Internship_Project01/app.js
@@ -1,3 +1,16 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express();
+mongoose.connect('mongodb://localhost:27017/twitter')
+    .then(() => { console.log('Connected to MongoDB...'); })
+    .catch((err) => { console.error('Could not connect to MongoDB...', err) })
+
+app.get('/', (req, res) => {
+    res.render('Welcome')
+})
+
+app.listen(3000, () => {
+    console.log('Server is running on port 3000')
+})
 
