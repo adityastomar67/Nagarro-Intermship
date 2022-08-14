e37d2c51cd1e8cfeb8b1d9b261f347702665ed51
Aditya Singh Tomar <adityastomar12@gmail.com>
2022-08-14 12:17:24 +0530

    New Files

--- Nagarro_Internship_Project01/app.js
+++ Nagarro_Internship_Project01/app.js
@@ -1,16 +1,25 @@
-const express = require('express')
-const mongoose = require('mongoose');
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
 
 const app = express();
-mongoose.connect('mongodb://localhost:27017/twitter')
-    .then(() => { console.log('Connected to MongoDB...'); })
-    .catch((err) => { console.error('Could not connect to MongoDB...', err) })
+mongoose.connect("mongodb://127.0.0.1:27017/twitter")
+  .then(() => {
+    console.log("Connected to MongoDB...");
+  })
+  .catch((err) => {
+    console.error("Could not connect to MongoDB...", err);
+  });
 
-app.get('/', (req, res) => {
-    res.render('Welcome')
-})
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "/views"));
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+  // res.send('Welcome')
+  res.render("home");
+});
 
 app.listen(3000, () => {
-    console.log('Server is running on port 3000')
-})
-
+  console.log("Server is running on port 3000");
+});
